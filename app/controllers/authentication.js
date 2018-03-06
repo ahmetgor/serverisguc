a8fb5d13ba0da7124dc67b4d227379a5eb43e720
ahmetgor <ahmet.gor@yandex.com>
2018-03-06 15:57:24 +0300

    ionic pro

--- app/controllers/authentication.js
+++ app/controllers/authentication.js
@@ -9,7 +9,7 @@ exports.login = function(req, res, next){
 
 exports.linkedPerson = function(req, res, next){
   console.log("gübe"+req.query.token);
-  request.get("https://api.linkedin.com/v1/people/~:(id,formatted-name,location,industry,summary,specialties,positions,picture-urls::(original),site-standard-profile-request,email-address)?format=json"
+  request.get("https://api.linkedin.com/v1/people/~:(id,formatted-name,location,industry,summary,specialties,positions,picture-urls::(original),picture-url,site-standard-profile-request,email-address)?format=json"
   , {
   'auth': {
     'bearer': req.query.token
